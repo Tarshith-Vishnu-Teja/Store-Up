6c210f011e2a9b58ed303b59d9f7d31ca266fb07
LAPTOP-H96I75IS\Krishna.R.K <krishna.rattihallikantharaj@sjsu.edu>
2017-10-27 12:41:02 -0700

    OCR Nodejs

--- Application/StoreUp-Server/routes/ocr.js
+++ Application/StoreUp-Server/routes/ocr.js
@@ -20,7 +20,8 @@ var gcloud = vision({
 
 
 router.post('/getImageOcr',function (res,req,next) {
-    var type = vision.v1.types.Feature.Type.TEXT_DETECTION;
+    //console.log(req.body.data);
+    /*var type = vision.v1.types.Feature.Type.TEXT_DETECTION;
     var featuresElement = { type:type};
     var features = [featuresElement];
 
@@ -29,7 +30,7 @@ router.post('/getImageOcr',function (res,req,next) {
         gcsImageUri : gcsImageUri
     };
     var image = {
-        content: fs.readFileSync('krk.jpg').toString('base64')
+        content: fs.readFileSync('unnamed.jpg').toString('base64')
     };
     var requestsElement = {
         image : image,
@@ -40,18 +41,81 @@ router.post('/getImageOcr',function (res,req,next) {
 
     gcloud.batchAnnotateImages({requests: requests}).then(function(results) {
         var response = results[0].responses[0].fullTextAnnotation;
+
         console.log(response);
         // doThingsWith(response)
-    })
-        .catch(function(err) {
+    }).catch(function(err) {
             console.error(err);
-        });
-    /*vision.detect('public/Images/krk.jpg',types,function (err,detections,apiResponse) {
-     if(err){
-     console.log(err.message);
-     }else{
-     console.log(detections);
-     }
-     })*/
-})
+        });*/
+     var type = vision.v1.types.Feature.Type.TEXT_DETECTION;
+     var typeLogo = vision.v1.types.Feature.Type.LOGO_DETECTION;
+     var featuresElement = { type:type};
+     var featuresElementLogo = { type:typeLogo};
+     var features = [featuresElement,featuresElementLogo];
+
+     var gcsImageUri = 'gs://storeup-7952a.appspot.com/images/image%3A30.jpg';
+     var source = {
+     gcsImageUri : gcsImageUri
+     };
+     var image = {
+     content: fs.readFileSync('unnamed.jpg').toString('base64')
+     };
+     var requestsElement = {
+     image : image,
+     features : features
+     };
+
+     var requests = [requestsElement];
+
+     gcloud.batchAnnotateImages({requests: requests}).then(function(results) {
+         var text = results[0].responses[0].fullTextAnnotation.text;
+         var logo = results[0].responses[0].logoAnnotations[0].description;
+         var address="";
+         console.log("Logo is: \n"+logo);
+         if(logo=="Target" || logo=="Target Corporation"){
+             var count=0;
+             for(var i=0;i<text.length;i++){
+                 if(count>1){
+                     break;
+                 }
+                 if(count==1){
+                     address+=text.charAt(i);
+                 }
+                 if(text.charAt(i)=='\n'){
+                     count++;
+                 }
+             }
+             console.log(address);
+         }
+         /*console.log("Logo is: \n"+logo);
+         console.log("Text is: \n"+text);*/
+
+         //fooProcess(data, successFooProcess, failureFooProcess);
+         //console.log();
+
+         //console.log(response);
+     // doThingsWith(response)
+     }).catch(function(err) {
+     console.error(err);
+     });
+});
+
+/*function successFooProcess(result) {
+    // do something
+}
+
+function failureFooProcess(error) {
+    // do something
+}
+
+function fooProcess(data, success, failure) {
+    if (true) {
+        var result = "";
+        success(result);
+    }
+    else {
+        var error = "";
+        failure(error);
+    }
+}*/
 module.exports = router;
