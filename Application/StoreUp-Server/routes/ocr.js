5d85442c46e9799a42b9c9a17b8c5b1c9fdb4f68
LAPTOP-H96I75IS\Krishna.R.K <krishna.rattihallikantharaj@sjsu.edu>
2017-10-27 23:23:35 -0700

    Adding Images for testing

--- Application/StoreUp-Server/routes/ocr.js
+++ Application/StoreUp-Server/routes/ocr.js
@@ -15,8 +15,12 @@ var gcloud = vision({
     projectId: 'storeup-164304',
 
     // The path to your key file:
-    keyFilename: 'StoreUp-6dfe8732b53c.json'
+    keyFilename: '../ServiceAccountKey/StoreUp-6dfe8732b53c.json'
 });
+
+var googleMapsClient = require('@google/maps').createClient({
+    key:'AIzaSyCLwIbvlz-UN3Z-E5nUgolJGCPLwnhcnHo'
+})
 
 
 router.post('/getImageOcr',function (res,req,next) {
@@ -53,25 +57,29 @@ router.post('/getImageOcr',function (res,req,next) {
      var featuresElementLogo = { type:typeLogo};
      var features = [featuresElement,featuresElementLogo];
 
-     var gcsImageUri = 'gs://storeup-7952a.appspot.com/images/image%3A30.jpg';
+     var gcsImageUri = 'gs://storeup-7952a.appspot.com/images/image%3A30';
      var source = {
-     gcsImageUri : gcsImageUri
+        gcsImageUri : gcsImageUri
      };
+     /*var image = {
+         source:source
+     };*/
      var image = {
-     content: fs.readFileSync('unnamed.jpg').toString('base64')
+     content: fs.readFileSync('../public/Images/Safeway.jpg').toString('base64')
      };
      var requestsElement = {
-     image : image,
-     features : features
+         image : image,
+         features : features
      };
 
      var requests = [requestsElement];
 
      gcloud.batchAnnotateImages({requests: requests}).then(function(results) {
          var text = results[0].responses[0].fullTextAnnotation.text;
-         var logo = results[0].responses[0].logoAnnotations[0].description;
+         //var logo = results[0].responses[0].logoAnnotations[0].description;
          var address="";
-         console.log("Logo is: \n"+logo);
+         console.log("Text is: \n"+text);
+         //console.log("Logo is: \n"+logo);
          if(logo=="Target" || logo=="Target Corporation"){
              var count=0;
              for(var i=0;i<text.length;i++){
@@ -86,15 +94,51 @@ router.post('/getImageOcr',function (res,req,next) {
                  }
              }
              console.log(address);
+         }else if(logo == "Walmart"){
+             var count=0;
+             for(var i=0;i<text.length;i++){
+                 if(count>5){
+                     break;
+                 }
+                 if(count==5){
+                     address+=text.charAt(i);
+                 }
+                 if(text.charAt(i)=='\n'){
+                     count++;
+                 }
+             }
+         }else if (logo == "Costco"){
+             var count=0;
+             for(var i=0;i<text.length;i++){
+                 if(count>3){
+                     break;
+                 }
+                 if(count==1 || count==2){
+                     address+=text.charAt(i);
+                 }
+                 if(text.charAt(i)=='\n'){
+                     count++;
+                 }
+             }
          }
-         /*console.log("Logo is: \n"+logo);
-         console.log("Text is: \n"+text);*/
+
+         console.log("Text is: \n"+text);
+         /*console.log("Store Name is :"+logo+" and the address is:"+address);
+         var addr = logo+" "+address;
+         console.log(addr);
+
+         googleMapsClient.geocode({
+             address: "Grocery Outlet #231 Downtown grocery out"
+         },function (err,response) {
+             if(!err){
+                 console.log(response.json.results[0].address_components);
+             }
+         });*/
+         /*console.log("Logo is: \n"+logo);*/
+         //console.log("Text is: \n"+text);
 
          //fooProcess(data, successFooProcess, failureFooProcess);
-         //console.log();
 
-         //console.log(response);
-     // doThingsWith(response)
      }).catch(function(err) {
      console.error(err);
      });
