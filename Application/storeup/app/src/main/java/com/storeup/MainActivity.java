1809a59cd498cd97be368db4fe47a74746e95ee8
LAPTOP-H96I75IS\Krishna.R.K <krishna.rattihallikantharaj@sjsu.edu>
2017-10-29 18:12:17 -0700

    Adding session storage to application

--- Application/storeup/app/src/main/java/com/storeup/MainActivity.java
+++ Application/storeup/app/src/main/java/com/storeup/MainActivity.java
@@ -71,11 +71,14 @@ public class MainActivity extends AppCompatActivity
     private Uri filePath;
     private StorageReference storageReference = FirebaseStorage.getInstance().getReference();
     //
+    AppSessionManager appSessionManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        appSessionManager=new AppSessionManager(getApplicationContext());
+        appSessionManager.checkLoginStatus();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
