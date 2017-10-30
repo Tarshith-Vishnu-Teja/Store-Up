c83011af3a92dd895edaee09a260d56e98ac40cc
Smitha Muthuvenkataramani <smitha.muthuvenkataramani@sjsu.edu>
2017-10-30 11:34:26 -0700

    Adding /userDetails API to get User profile information

--- Application/StoreUp-Server/routes/users.js
+++ Application/StoreUp-Server/routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var db = require('./connectdb')();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/userDetails', function(req, res, next) {
+    console.log("Comes here");
+    var email = req.body.email;
+
+    console.log(email);
+
+    db.query('select * from user_details where email = ?', [email], function(err, rows, fields) {
+        if (err) throw err;
+
+        if (rows.length > 0) {
+            res.json({success: "1", username: rows[0], message: "user details retrieved"});
+        }else {
+            res.json({success: "0", message: "error retrieving user details"});
+        }
+    })
+
 });
 
 module.exports = router;
