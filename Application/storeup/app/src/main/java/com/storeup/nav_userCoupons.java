8c18536eae723c9763b4214f26b82f18602b4e88
nikhildhamija24 <nikhil.dhamija24@gmail.com>
2017-11-10 12:30:50 -0800

    Refactor

--- Application/storeup/app/src/main/java/com/storeup/nav_userCoupons.java
+++ Application/storeup/app/src/main/java/com/storeup/nav_userCoupons.java
@@ -2,8 +2,6 @@ package com.storeup;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -24,14 +22,6 @@ public class nav_userCoupons extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -102,6 +92,10 @@ public class nav_userCoupons extends AppCompatActivity
                 Intent faqs = new Intent(nav_userCoupons.this, nav_faqs.class);
                 startActivity(faqs);
                 //fragment = new Faqs();
+                break;
+            case R.id.scan_receipts:
+                Intent i = new Intent(nav_userCoupons.this, ScanImageActivity.class);
+                startActivity(i);
                 break;
         }
 
